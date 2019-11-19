d253c8c7cc525e1047ad0335e241399ead6e34fd
kevin lam <kevinlam07696@gmail.com>
2019-11-19 16:05:04 -0500

    Added Logout btn

--- app/src/main/java/com/example/parseinstagram/LoginActivity.java
+++ app/src/main/java/com/example/parseinstagram/LoginActivity.java
@@ -13,6 +13,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -21,6 +22,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText etUsername;
     private EditText etPassword;
     private Button btnLogin;
+    private Button btnSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +39,7 @@ public class LoginActivity extends AppCompatActivity {
         etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
         btnLogin = findViewById(R.id.btnLogin);
+        btnSignup = findViewById(R.id.btnSignup);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,6 +47,15 @@ public class LoginActivity extends AppCompatActivity {
                 String username = etUsername.getText().toString();
                 String password = etPassword.getText().toString();
                 login(username, password);
+            }
+        });
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                signup(username, password);
             }
         });
 
@@ -62,6 +74,27 @@ public class LoginActivity extends AppCompatActivity {
                 // TODO: navigate to new activity if the user has signed properly.
                 Log.d(TAG, "Login successful");
                 goMainActivity();
+            }
+        });
+    }
+
+    private void signup(String username, String password) {
+        ParseUser user = new ParseUser();
+        // Set core properties
+        user.setUsername(username);
+        user.setPassword(password);
+        // Invoke signUpInBackground
+        user.signUpInBackground(new SignUpCallback() {
+            public void done(ParseException e) {
+                if (e == null) {
+                    // Hooray! Let them use the app now.
+                    Log.d(TAG, "Sign Up Successful");
+                    goMainActivity();
+                } else {
+                    // Sign up didn't succeed. Look at the ParseException
+                    Log.e(TAG, "Issue with Sign Up", e);
+                    e.printStackTrace();
+                }
             }
         });
     }
